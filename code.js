12ed3420b5ade2f70a3e617578431582a0ede57b
paxluporum <paxluporum@gmail.com>
2025-11-19 11:14:58 -0500

    end of class

--- code.js
+++ code.js
@@ -1,23 +1,22 @@
+import { Game } from "./game.js";
+import { GameOver } from "./gameOver.js";
+import { TitleScreen } from "./titleScreen.js";
 import { Toolbox } from "./toolbox.js";
 
 let canvas = document.getElementById("myCanvas");
 let pencil = canvas.getContext("2d"); // This gives you the drawing context, like a pencil
 let toolbox = new Toolbox();
 
-// let myFavoriteLetters = ["a", "b", "c"];
-console.log(toolbox.getRandomColor());
 
-pencil.fillStyle = toolbox.getRandomColor();
-pencil.fillRect(50, 50, 100, 100);
+//make some states to go to.
+let game = new Game();
+let gameOver = new GameOver();
+let titleScreen = new TitleScreen
 
-// let counts = [0,0,0];
-// for(let i = 0; i < 100; i++) {
-// //     let randomIndex = getRandomIndex(myFavoriteLetters);
-// //     counts[randomIndex] += 1;
+let state = titleScreen;
 
-//     console.log(toolbox.getRandomItem(myFavoriteLetters));
-// }
+function gameLoop() {
+    state.update();
+}
 
-
-// let shuffled = toolbox.shuffleArray(myFavoriteLetters);
-// console.log(shuffled);
+setInterval(gameLoop, 1000 / 60);
---
+++ gameOver.js
@@ -0,0 +1,6 @@
+export class GameOver {
+
+    update() {
+        
+    }
+}
